9720df5630f3a879b39ff46dff44baf9a678efc8
Oriol Burgaya <oriolbv@gmail.com>
2020-02-22 11:49:35 +0100

    ExitGame script added and attached to ExitGameButton

---
+++ Assets/Scripts/GameExit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameExit : MonoBehaviour
+{
+    public void ExitGame() 
+    {
+        Debug.Log("Exit pressed!");
+        Application.Quit();
+    }
+}
