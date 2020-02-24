df5501f3d7bfb51225e5f9c38b2a78e7f1c0971f
Oriol Burgaya <oriolbv@gmail.com>
2020-02-24 07:32:50 +0100

    Start player selected randomly when user starts a new game

---
+++ Assets/Scripts/GamplayManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GamplayManager : MonoBehaviour
+{
+    private void Start()
+    {
+        int player = GetRandomPlayer();
+        Debug.Log("RandomPlayer: " + player.ToString());
+
+        //currentNode = StoryFiller.FillStory();
+        //HistoryText.text = string.Empty;
+        //FillUi();
+    }
+
+    private int GetRandomPlayer()
+    {
+        // Return a random integer number between 1 [inclusive] and 3 [exclusive]
+        return Random.Range(1, 3);
+    }
+
+}
