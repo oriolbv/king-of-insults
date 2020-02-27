943221b9cf9bab9a1dfc7bc175c24dddf0eca875
Oriol Burgaya <oriolbv@gmail.com>
2020-02-27 21:26:13 +0100

    Insults/Answers space filled with some phrases

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameplayManager : MonoBehaviour
 {
+    public Transform OptionButtons;
+    public GameObject ButtonPrefab;
+
     private void Start()
     {
         int player = GetRandomPlayer();
@@ -21,7 +25,53 @@ public class GameplayManager : MonoBehaviour
         }
 
         Debug.Log("Turn: " + gs.actualGameplayState.ToString());
+
+        FillUI();
     }
+
+    public void FillUI()
+    {
+
+        //foreach (Transform child in OptionButtons.transform)
+        //{
+        //    Destroy(child.gameObject);
+        //}
+
+        var isLeft = true;
+        var height = 50.0f;
+        var index = 0;
+        var insults = InsultFiller.FillInsults();
+        foreach (var insult in insults)
+        {
+            Debug.Log(insult.Answer);
+
+            var buttonAnswerCopy = Instantiate(ButtonPrefab, OptionButtons, true);
+
+            var x = buttonAnswerCopy.GetComponent<RectTransform>().rect.x * 1.3f;
+            buttonAnswerCopy.GetComponent<RectTransform>().localPosition = new Vector3(isLeft ? x : -x, height, 0);
+
+            if (!isLeft)
+                height += buttonAnswerCopy.GetComponent<RectTransform>().rect.y * 3.0f;
+            isLeft = !isLeft;
+
+            FillListener(buttonAnswerCopy.GetComponent<Button>(), index);
+
+            buttonAnswerCopy.GetComponentInChildren<Text>().text = insult.Answer;
+
+            index++;
+        }
+    }
+
+    private void FillListener(Button button, int index)
+    {
+        button.onClick.AddListener(() => { InsultSelected(index); });
+    }
+
+    private void InsultSelected(int index)
+    {
+        Debug.Log("Index button: " + index.ToString());
+    }
+
 
     private int GetRandomPlayer()
     {
