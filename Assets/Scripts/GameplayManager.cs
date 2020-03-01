1f5637a2073c43759a8ddd99557bd96b639b8db0
Oriol Burgaya <oriolbv@gmail.com>
2020-03-01 11:15:38 +0100

    One turn gameflow implemented and Sprites folder added

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -14,11 +14,16 @@ public class GameplayManager : MonoBehaviour
     private InsultNode[] _insults;
     private int _insultIdx = -1;
     private int _answerIdx = -1;
+    private int _firstTurn = -1;
+
+    // Lives
+    private int _playerLives = 3;
+    private int _enemyLives = 3;
+
 
     private void Start()
     {
         int player = GetRandomPlayer();
-        Debug.Log("RandomPlayer: " + player.ToString());
 
         // Fill all the insults and answers to use during the duel
         _insults = InsultFiller.FillInsults();
@@ -38,7 +43,7 @@ public class GameplayManager : MonoBehaviour
             WriteEnemyOption();
         }
 
-        Debug.Log("Turn: " + _gs.actualGameplayState.ToString());
+        _firstTurn = player;
         
         FillUI();
 
@@ -48,10 +53,7 @@ public class GameplayManager : MonoBehaviour
     public void FillUI()
     {
         // Fill down space with all the Insults/Answers, depending on the current turn.
-        foreach (Transform child in OptionButtons.transform)
-        {
-            Destroy(child.gameObject);
-        }
+        RemoveUI();
 
         var height = 50.0f;
         var index = 0;
@@ -71,23 +73,31 @@ public class GameplayManager : MonoBehaviour
 
             FillListener(insultText.GetComponent<Button>(), index);
 
-            if (_gs.actualGameplayState is PlayerTurnState) insultText.GetComponentInChildren<Text>().text = insult.Insult;
+            if (_firstTurn == 1) insultText.GetComponentInChildren<Text>().text = insult.Insult;
             else insultText.GetComponentInChildren<Text>().text = insult.Answer;
 
             index++;
         }
     }
 
-    public void WritePlayerOption(string msg)
+    public void RemoveUI()
     {
+        foreach (Transform child in OptionButtons.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
 
+    public void WritePlayerOption(int index)
+    {
+        PlayerText.GetComponentInChildren<Text>().text = ((_gs.actualGameplayState is PlayerTurnState && _insultIdx == -1) ? _insults[index].Insult : _insults[index].Answer);
     }
 
     public void WriteEnemyOption() 
     {
         int insultIdx = Random.Range(0, _insults.Length);
         EnemyText.GetComponentInChildren<Text>().text = (_gs.actualGameplayState is EnemyTurnState ? _insults[insultIdx].Insult : _insults[insultIdx].Answer);
-        if (_gs.actualGameplayState is PlayerTurnState)
+        if (_firstTurn == 1)
         {
             _answerIdx = insultIdx;
             CheckRoundWinner();
@@ -103,6 +113,37 @@ public class GameplayManager : MonoBehaviour
     public void CheckRoundWinner()
     {
         Debug.Log("AND THE WINNER IS....");
+        if (_insultIdx == _answerIdx)
+        {
+            // Wins the one that answered the insult
+            if (_firstTurn == 1)
+            {
+                // Enemy wins
+                Debug.Log("The Enemy");
+            }
+            else
+            {
+                // Player wins
+                Debug.Log("The Player");
+            }
+        }
+        else
+        {
+            // Wins the one that insulted first
+            if (_firstTurn == 1)
+            {
+                // Player wins
+                Debug.Log("The Player");
+            }
+            else
+            {
+                // Enemy wins
+                Debug.Log("The Enemy");
+            }
+
+        }
+
+        _firstTurn = -1;
         _insultIdx = -1;
         _answerIdx = -1;
     }
@@ -116,11 +157,14 @@ public class GameplayManager : MonoBehaviour
     private void InsultSelected(int index)
     {
         Debug.Log("Index button: " + index.ToString());
-        PlayerText.GetComponentInChildren<Text>().text = ((_gs.actualGameplayState is PlayerTurnState && _insultIdx == -1) ? _insults[index].Insult : _insults[index].Answer);
+        WritePlayerOption(index);
         if (_insultIdx == -1)
         {
             _insultIdx = index;
-            FillUI();
+            // Transition to EnemyTurnState
+            _gs.actualGameplayState.ToEnemyTurnState();
+            // Enemy will choose randomly his first insult
+            WriteEnemyOption();
         } 
         else
         {
